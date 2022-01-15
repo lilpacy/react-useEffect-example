7229d82cae47af63cdb809d8a75f00ff92a2217d
pacy <revivedtomorrow@gmail.com>
2022-01-15 23:40:50 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [z, setZ] = useState(0);
+  const incrementX = () => {
+    setX(x + 1);
+  };
+  useEffect(() => {
+    setY(y + 1);
+  }, [x]);
+  useEffect(() => {
+    setZ(z + 1);
+  }, [y]);
+  return (
+    <div className="App">
+      <div>
+        {x},{y},{z}
+      </div>
+      <button onClick={incrementX}>click</button>
+    </div>
+  );
+}
